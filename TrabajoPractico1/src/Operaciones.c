645202820a13b5d8301b5dfc25e13b2eb094955b
Tomas <tomas_costillachavez@hotmail.com>
2019-08-31 13:39:49 -0300

    Se agregan bibliotecas .h y .c con las operaciones basicas de la calculadora

---
+++ TrabajoPractico1/src/Operaciones.c
@@ -0,0 +1,34 @@
+/*
+ * Operaciones.c
+ *
+ *  Created on: 31 ago. 2019
+ *      Author: Tomas Costilla
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumaValores(int valorX,int valorY,int resultado)
+{
+
+}
+
+int restaValores(int valorX,int valorY,int resultado);
+{
+
+}
+
+int multiplicaValores(int valorX,int valorY,int resultado);
+{
+
+}
+
+int divideValores(int valorX,int valorY,int resultado);
+{
+
+}
+
+int factorial(int valorX,int valorY,int resultado);
+{
+
+}
---
+++ TrabajoPractico1/src/ProtOperaciones.h
@@ -0,0 +1,19 @@
+/*
+ * ProtOperaciones.h
+ *
+ *  Created on: 31 ago. 2019
+ *      Author: Tomas Costilla
+ */
+
+#ifndef PROTOPERACIONES_H_
+#define PROTOPERACIONES_H_
+
+
+int sumaValores(int valorX,int valorY,int resultado);
+int restaValores(int valorX,int valorY,int resultado);
+int multiplicaValores(int valorX,int valorY,int resultado);
+int divideValores(int valorX,int valorY,int resultado);
+int factorial(int valorX,int valorY,int resultado);
+
+
+#endif /* PROTOPERACIONES_H_ */
