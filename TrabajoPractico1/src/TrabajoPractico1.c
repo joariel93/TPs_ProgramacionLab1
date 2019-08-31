0e0101cc78583efeb477c3433d9906b1825cffc9
Tomas <tomas_costillachavez@hotmail.com>
2019-08-31 13:11:05 -0300

    Header y opciones de Menu

--- TrabajoPractico1/src/TrabajoPractico1.c
+++ TrabajoPractico1/src/TrabajoPractico1.c
@@ -15,10 +15,18 @@ int main(void)
 {
 	int x;
 	int y;
+	int opcion;
 
 	printf("****************************************************");
 	printf("\n***************CALCULADORA HECHA EN #C**************");
 	printf("\n****************************************************");
+	printf("\n\nEliga una opcion de Calculo...");
+	printf("\n\n1-Suma");
+	printf("\n2-Resta");
+	printf("\n3-Multiplicacion");
+	printf("\n4-Division");
+	printf("\n5-Factorial");
+
 
 	return 0;
 }
