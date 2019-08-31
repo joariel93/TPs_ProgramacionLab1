de24db0399f0986cd97bd59fdd5025c56a3efe28
Tomas <tomas_costillachavez@hotmail.com>
2019-08-31 13:03:27 -0300

    Primer commit, Titulo

---
+++ TrabajoPractico1/src/TrabajoPractico1.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : TrabajoPractico1.c
+ Author      : Tomas Costilla Chavez
+ Version     :
+ Copyright   : Tomas Costilla Chavez
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int x;
+	int y;
+
+	printf("****************************************************");
+	printf("\n***************CALCULADORA HECHA EN #C**************");
+	printf("\n****************************************************");
+
+	return 0;
+}
