414d21fd746818f85614d32e050ba6687f7b18a9
Tomas <tomas_costillachavez@hotmail.com>
2019-09-05 19:39:01 -0300

    Trabajo Practico 1

--- TrabajoPractico1/src/Operaciones.c
+++ TrabajoPractico1/src/Operaciones.c
@@ -8,24 +8,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *  \brief Recibe el valor A, Valor B y calcula la Suma
+ *  \param Valor A
+ *  \param Valor B
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return La suma de los Enteros
+ */
 int sumaValores(int valorX,int valorY,int* presultado)
 {
 	*presultado = valorX+valorY;
 	return *presultado;
 }
 
+/**
+ *  \brief Recibe el valor A, Valor B y calcula la Resta
+ *  \param Valor A
+ *  \param Valor B
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return La Resta de los Enteros
+ */
 int restaValores(int valorX,int valorY,int* presultado)
 {
 	*presultado = valorX-valorY;
 	return *presultado;
 }
 
+/**
+ *  \brief Recibe el valor A, Valor B y calcula la Multiplicacion
+ *  \param Valor A
+ *  \param Valor B
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return La Multiplicacion de los Enteros
+ */
 int multiplicaValores(int valorX,int valorY,int* presultado)
 {
 	*presultado = valorX*valorY;
 	return *presultado;
 }
 
+/**
+ *  \brief Recibe el valor A, Valor B y calcula la Division
+ *  \param Valor A
+ *  \param Valor B
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return La Division de los Enteros
+ */
 int divideValores(int valorX,int valorY,int* presultado)
 {
 
@@ -33,6 +61,12 @@ int divideValores(int valorX,int valorY,int* presultado)
 	return *presultado;
 }
 
+/**
+ *  \brief Recibe el valor A  y calcula el factorial
+ *  \param Valor A
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return Imprime por consola el Factorial del Valor A
+ */
 int factorialX(int valorX,int* pfact)
 {
 	int i;
@@ -45,6 +79,13 @@ int factorialX(int valorX,int* pfact)
 	fflush(stdout);
 	return 0;
 }
+
+/**
+ *  \brief Recibe el valor B  y calcula el factorial
+ *  \param Valor B
+ *  \param Puntero a la variable "Resultado" de la funcion Main
+ *  \return Imprime por consola el Factorial del Valor B
+ */
 int factorialY(int valorY,int* pfact)
 {
 	int i;
--- TrabajoPractico1/src/ProtOperaciones.h
+++ TrabajoPractico1/src/ProtOperaciones.h
@@ -8,12 +8,40 @@
 #ifndef PROTOPERACIONES_H_
 #define PROTOPERACIONES_H_
 
-
+/**
+ * \brief Recibe dos Valores por parametros y devuelve la Suma
+ * \param Valor A y B ingresados por el usuario, puntero a la variable Resultado del Main
+ */
 int sumaValores(int valorX,int valorY,int* presultado);
+
+/**
+ * \brief Recibe dos Valores por parametros y devuelve la Resta
+ * \param Valor A y B ingresados por el usuario, puntero a la variable "Resultado" del Main
+ */
 int restaValores(int valorX,int valorY,int* presultado);
+
+/**
+ * \brief Recibe dos Valores por parametros y devuelve la Multiplicacion
+ * \param Valor A y B ingresados por el usuario, puntero a la variable Resultado del Main
+ */
 int multiplicaValores(int valorX,int valorY,int* presultado);
+
+/**
+ * \brief Recibe dos Valores por parametros y devuelve la Division
+ * \param Valor A y B ingresados por el usuario, puntero a la variable Resultado del Main
+ */
 int divideValores(int valorX,int valorY,int* presultado);
+
+/**
+ * \brief Recibe el valor A por parametro y devuelve el factorial
+ * \param Valor A y puntero a la variable Resultado del Main
+ */
 int factorialX(int valorX,int* pfact);
+
+/**
+ * \brief Recibe el valor B por parametro y devuelve el factorial
+ * \param Valor B y puntero a la variable Resultado del Main
+ */
 int factorialY(int valorY,int* pfact);
 
 
